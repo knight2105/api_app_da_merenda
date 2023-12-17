f59969d5e85f3d350423018f69a1850a546f7052
knight <141058681+knight2105@users.noreply.github.com>
2023-12-17 13:10:03 -0300

    Add files via upload

--- app_merenda.py
+++ app_merenda.py
@@ -1,16 +1,11 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, Request, HTTPException
 from pydantic import BaseModel
 from hashlib import sha256
 import mysql.connector
 import qrcode
 from fastapi.responses import FileResponse
+from datetime import datetime
 import os
-
-class Aluno(BaseModel):
-    matricula: int
-    nomes: str
-    id_curso: int
-
 
 app = FastAPI(swagger_ui_parameters={"deepLinking": False})
 router = APIRouter(prefix="/principal")
@@ -21,14 +16,13 @@ try:
         host='localhost',
         user='Victor',
         password='V1c$or#456',
-        database='app_merenda',
+        database='app_merenda2',
     )
 except mysql.connector.Error as err:
     print(f"Erro ao conectar ao banco de dados: {err}")
 
+#gerar o qr code
 app.include_router(router)
-
-# Função para gerar um QR Code e salvar em um arquivo
 def generate_qr_code(data: str, file_path: str):
     qr = qrcode.QRCode(
         version=1,
@@ -39,67 +33,172 @@ def generate_qr_code(data: str, file_path: str):
     qr.add_data(data)
     qr.make(fit=True)
     img = qr.make_image(fill_color="black", back_color="white")
-
-    # Certifica-se de que o diretório existe antes de salvar
     directory = os.path.dirname(file_path)
     if not os.path.exists(directory):
         os.makedirs(directory)
-
     img.save(file_path)
 
-@app.post("/alunos/cadastrar")
-async def cadastrar_aluno(aluno: Aluno):
-    print(aluno)
-    matricula = aluno.matricula
-    nomes = aluno.nomes
-    id_curso = aluno.id_curso
+#pega o qr code do aluno 
+@app.get("/qr_code/{user_id}")
+def get_qr_code(user_id: int):
+    qr_code_file_path = f"qr_codes/{user_id}_qrcode.png"
+    return FileResponse(qr_code_file_path, media_type="image/png", filename=f"qrcode_{user_id}.png")
 
+#função para verificar hora
+def verificar_horas():
+	hora_atual = datetime.now().strftime('%H')
+	return hora_atual
+#função para o dia 
+def ver_dia():
+	dias_da_semana = {
+	    'Monday': 'segunda-feira',
+	    'Tuesday': 'terça-feira',
+	    'Wednesday': 'quarta-feira',
+	    'Thursday': 'quinta-feira',
+	    'Friday': 'sexta-feira',
+	    'Saturday': 'sábado',
+	    'Sunday': 'domingo'
+	}
+	data_e_hora_atual = datetime.now()
+	dia_da_semana = data_e_hora_atual.strftime('%A')
+	if dia_da_semana in dias_da_semana:
+	     return dias_da_semana[dia_da_semana]
+
+
+
+@app.get("/home/usuario/autenticar/{id_nome}/{senha}")
+def verificar(id_nome, senha):
+    if mydb:  
+        cursor = mydb.cursor()
+        query_usuario = "SELECT nome FROM usuarios WHERE id = %s;"
+        cursor.execute(query_usuario, (senha,))
+        result = cursor.fetchone()
+        if result:
+            return {"Bem-vindo"}
+        else:
+            return {"Usuário não encontrado"}
+
+
+
+class Usuario2(BaseModel):
+    id_usuario: int
+    nome_usuario: str
+    senha: str
+
+@app.post("/usuarios/cadastrar")
+async def cadastrar(usuario: Usuario2):
+    id_usuario = usuario.id_usuario
+    nome_usuario = usuario.nome_usuario
+    senha = usuario.senha
     if mydb:
         cursor = mydb.cursor()
-
-        # Verifica se a matrícula já existe
-        query = "SELECT matricula FROM alunos_info where matricula = %s;"
-        cursor.execute(query, (matricula,))
+        query = "SELECT id FROM usuarios where id = %s;"
+        cursor.execute(query, (id_usuario,))
         verificar = cursor.fetchone()
-
         if verificar:
-            return {'message': 'Aluno já cadastrado'}
+            return {'message': 'Usuário já existe'}
         else:
-            # Adiciona o gerador de QR Code
-            qr_code_data = f"Matrícula: {matricula}\nNomes: {nomes}\nCurso: {id_curso}"
-            qr_code_file_path = f"qr_codes/{matricula}_qrcode.png"
-            generate_qr_code(qr_code_data, qr_code_file_path)
-
-            # Insere os dados do aluno no banco de dados
-            query = "INSERT INTO alunos_info (matricula, nomes, QR, id_curso) VALUES (%s, %s, %s, %s);"
-            cursor.execute(query, (matricula, nomes, qr_code_file_path,id_curso))
+        
+            query = "INSERT INTO usuarios (id, nome,senha) VALUES (%s, %s, %s);"
+            cursor.execute(query, (id_usuario, nome_usuario, senha,))
             mydb.commit()
             cursor.close()
-
-            return {'message': 'Aluno cadastrado com sucesso', 'qr_code_path': qr_code_file_path}
+            return {'message': 'Cadastrado com sucesso'}
     else:
-        return {'message': 'Você não está autorizado a cadastrar um novo aluno.'}
+        return {'message': 'Você não está autorizado a cadastrar um novo usuário.'}
 
-
-@app.get("/buscar_aluno/{matricula}")
-def get_buscar_aluno(matricula: int):
+#parte dos alunos 
+class Aluno(BaseModel):
+    matricula: int
+    nomes: str
+@app.post("/alunos/cadastrar")
+async def cadastrar(aluno: Aluno):
+    matricula = aluno.matricula
+    nome = aluno.nomes
     if mydb:
         cursor = mydb.cursor()
+        qr_code_data = f"{matricula}"
+        query = "INSERT INTO alunos_info (matricula,nome) VALUES (%s, %s)"
+        cursor.execute(query, (matricula, nome,))
+        mydb.commit()
+        qr_code_file_path = f"qr_codes/{matricula}_qrcode.png"
+        generate_qr_code(qr_code_data, qr_code_file_path)
+        return {'message': "aluno criado" 'QR Code criado com sucesso', 'qr_code_path': qr_code_file_path}
 
-       
-        query = "SELECT * FROM alunos_info where matricula = %s;"
-        cursor.execute(query, (matricula,))
-        dados_aluno = cursor.fetchone()
-        cursor.close()
-
-        if dados_aluno:
-            return {"informacoes": dados_aluno}
+@app.get("/home/autenticar/{aluno_id}")
+def verificar(aluno_id):
+    if mydb:
+        cursor = mydb.cursor()
+        query = "SELECT nome FROM alunos_info WHERE matricula = %s;"
+        cursor.execute(query, (aluno_id,))
+        nome = cursor.fetchone()
+        if nome:
+            apenas_hora = "0"
+            hora_completa = verificar_horas()
+            for i in range(2):
+                apenas_hora += str(int(hora_completa.split(':')[i]))
+                query_turno = "SELECT t.horas FROM turnos t JOIN alunos_tem_turnos att ON t.id = att.id_turno JOIN alunos_info as ai ON ai.matricula = att.id_alunos WHERE HOUR(t.horas) = %s AND ai.matricula = %s;"
+                cursor.execute(query_turno, (apenas_hora, aluno_id,))
+                turno_valido = cursor.fetchone()
+                if turno_valido:
+                    hoje = ver_dia()
+                    query_disciplina = "SELECT d.nome FROM disciplinas d JOIN alunos_info JOIN alunos_tem_disciplinas atd ON atd.id_alunos = %s AND d.id = atd.id_disciplinas JOIN disciplinas_tem_dias_tem_turnos dtt ON d.id = dtt.id_disciplinas JOIN dias di ON di.id = dtt.id_dias JOIN turnos t ON t.id = dtt.id_turno WHERE di.nome = %s AND HOUR(t.horas) = %s AND alunos_info.matricula = atd.id_alunos;"
+                    cursor.execute(query_disciplina, (aluno_id, hoje, apenas_hora,))
+                    disciplina_valida = cursor.fetchone()
+                    if disciplina_valida:
+                        query_merenda = "SELECT aluno_merendou FROM alunos_info WHERE matricula = %s;"
+                        cursor.execute(query_merenda, (aluno_id,))
+                        merendou = cursor.fetchone()
+                        if merendou == 0:
+                            return {"pelo que eu sei, já pode ao menos tentar merendar"}
+                        else:
+                            return {"aluno já merendou, liberar mesmo assim?"}
+                    else:
+                        return {"mensagem": "aluno sem matrícula neste turno, liberar mesmo assim?"}
+                else:
+                    return {"mensagem": "aluno sem turno correspondente, liberar mesmo assim?"}
         else:
-            return {"message": "Aluno não encontrado"}
+            return {"cadastro não encontrado"}
     else:
-        return {"message": "Você está offline ;("}
+        return {"Não foi possível se conectar ao banco de dados ;("}
 
-@app.get("/qr_code/{matricula}")
-def get_qr_code(matricula: int):
-    qr_code_file_path = f"qr_codes/{matricula}_qrcode.png"
-    return FileResponse(qr_code_file_path, media_type="image/png", filename=f"qrcode_{matricula}.png")
+class Controle(BaseModel):
+    id: int
+
+@app.post("/home/registrar_merenda")
+async def aluno_merendou(controle: Controle):
+    id = controle.id
+    query = "UPDATE alunos_info SET aluno_merendou = 1 WHERE matricula = %s;" 
+    if mydb:
+        try:
+            cursor = mydb.cursor()
+            cursor.execute(query, (id,))
+            mydb.commit()
+            cursor.close()
+            return {"mensagem": "ja pode ao mostrar"}
+        except Exception as e:
+            return HTTPException(403, detail={"mensagem": f"Erro ao registrar merenda: {e}"})
+    else:
+        return {"mensagem": "Não foi possível se conectar ao banco de dados"}
+
+
+
+def verificar_data():
+	data = datetime.now().strftime("%Y-%m-%d")
+	return data
+#autorização especial
+class Autorizacao(BaseModel):
+    id_aluno: str
+    id_usuario: str
+    id_turno: str
+    motivo: str
+    data: str
+
+@app.post("/home/registrar_merenda_autorizacao")
+async def autorizar_aluno(autorizacao: Autorizacao):
+    query = "INSERT INTO autorização_especial (id_aluno, id_usuario, id_turno, motivo, data) VALUES (%s, %s, %s, %s, %s);"
+    cursor = mydb.cursor()
+    cursor.execute(query, (autorizacao.id_aluno, autorizacao.id_usuario, autorizacao.id_turno, autorizacao.motivo, autorizacao.data))
+    mydb.commit()
+    cursor.close()
+    return {"mensagem": "autorização concedida, já pode ao mostrar"}
